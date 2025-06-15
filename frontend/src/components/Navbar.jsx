6424911e455985c04fa0926fd5f13acb4a3e0520
muhammadsherazsandila <msd.sheraz046@gmail.com>
2025-06-15 20:32:25 +0500

    Updating UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -61,7 +61,7 @@ function Navbar() {
               className="flex items-center cursor-pointer hover:scale-105 transition duration-300"
               onClick={() => navigate("/dashboard")}
             >
-              <imgs
+              <img
                 src={
                   isLoggedIn && user.profilePicture
                     ? user.profilePicture

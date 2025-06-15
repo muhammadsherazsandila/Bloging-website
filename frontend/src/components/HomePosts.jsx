ea7f7bb9b03d92b782c8d0713209645aa557d57a
muhammadsherazsandila <msd.sheraz046@gmail.com>
2025-06-15 21:09:24 +0500

    Updating UI

--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { usePost } from "../contexts/PostContext";
 import BlogCard from "./BlogCard";
+import { CircularProgress } from "@mui/material";
 
 function HomePosts() {
   const { posts, setPosts } = usePost();
   return (
     <div className="flex flex-col gap-6 items-center md:p-4 lg:p-8 xl:p-12 p-2">
       <h2 className="text-3xl font-bold mb-2">Latest Posts</h2>
-      {posts.map((post, index) => (
-        <BlogCard key={index} post={post} />
-      ))}
+      {posts.length > 0 ? (
+        posts.map((post, index) => <BlogCard key={index} post={post} />)
+      ) : (
+        <p className="flex items-center gap-2">
+          <CircularProgress />
+          <span>Loading...</span>
+        </p>
+      )}
     </div>
   );
 }

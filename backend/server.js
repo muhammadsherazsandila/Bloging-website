cda74d37966a503bcc3c4b27cc7f5b9a45a3453c
muhammadsherazsandila <msd.sheraz046@gmail.com>
2025-07-17 11:58:06 +0500

    updating

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://blogorabblogs.vercel.app",
+  "https://blogorablogs.vercel.app/",
 ];
 
 app.use(

a334d69f7961e0cd245f1ab0342a41f399fcabbc
muhammadsherazsandila <msd.sheraz046@gmail.com>
2025-07-17 10:58:42 +0500

    updating

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,7 @@ app.use("/api", mainRouter);
 app.use("/user", userRouter);
 app.use("/post", postsRouter);
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen( PORT, () => console.log("Server is running on port 5000"));
 export default app;
